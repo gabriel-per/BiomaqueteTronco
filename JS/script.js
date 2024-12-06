9d4dab198f892be5f1c27077efd5ec60c10dba20
gabriel-per <117866610+gabriel-per@users.noreply.github.com>
2024-12-06 10:21:03 -0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -11,21 +11,24 @@ window.onscroll = () =>{
 }
 
 const cell = document.getElementById("morphingCell");
+const nucleus = document.getElementById("nuclei");
 
-window.addEventListener("scroll", () => {
-    const scrollPos = window.scrollY / (document.body.offsetHeight - window.innerHeight);
+    window.addEventListener("scroll", () => {
+      const scrollPos = window.scrollY / 3500;
 
-    if (scrollPos < 0.2) {
-    cell.className = "cell"; // Stem cell
-    } else if (scrollPos < 0.4) {
-    cell.className = "cell muscle-cell"; // Muscle cell
-    } else if (scrollPos < 0.6) {
-    cell.className = "cell blood-cell"; // Blood cell
-    } else if (scrollPos < 0.8) {
-    cell.className = "cell nerve-cell"; // Nerve cell
-    } else if (scrollPos < 1.0) {
-    cell.className = "cell adipose-cell"; // Adipose cell
-    } else {
-    cell.className = "cell skin-cell"; // Skin cell
-    }
-});
+      if (scrollPos < 0.2) {
+        cell.className = "cell"; // Stem cell
+      } else if (scrollPos < 0.4) {
+        cell.className = "cell muscle-cell"; // Muscle cell
+      } else if (scrollPos < 0.6) {
+        cell.className = "cell blood-cell"; // Blood cell
+      } else if (scrollPos < 0.8) {
+        cell.className = "cell nerve-cell"; // Nerve cell
+      } else if (scrollPos < 1) {
+        cell.className = "cell adipose-cell"; // Adipose cell
+      } else if (scrollPos >= 1 && scrollPos < 1.1) {
+        cell.className = "cell skin-cell"; // Skin cell
+      } else {
+        cell.className = "cell empty-cell";
+      }
+    });
